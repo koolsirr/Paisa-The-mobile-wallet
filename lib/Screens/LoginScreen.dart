28ec25564d24ec42da4fc66b460668ca555c8335
koolsirr <bishalmdr12@gmail.com>
2023-07-02 18:31:05 +0545

    welcome,login and register pages added

---
+++ lib/Screens/LoginScreen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image:DecorationImage(
+              image: AssetImage('assets/images/bg.png'),fit: BoxFit.cover)
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children:[
+            SingleChildScrollView(
+              child: Container(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height*0.52,
+                      left: 20,
+                      right: 20
+                  ),
+                  child:
+                  Column(
+                    children: [
+                      const Text('Sign In',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'UserName',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(13)
+                            )
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(13)
+                            )
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: const Color(0xff473c38),
+                            child: IconButton(
+                              color: Colors.white,
+                              onPressed: (){},
+                              icon: const Icon(Icons.arrow_forward_ios_rounded),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(onPressed: (){}, child: const Text(
+                            'Forgot Password',
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontSize: 18,
+                                color: Color(0xff4c505b)
+                            ),))
+                        ],
+                      )
+                    ],
+                  )
+              ),
+            )
+
+          ],
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/Screens/WelcomeScreen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:paisa_majorproject/Screens/RegisterScreen.dart';
+
+import 'LoginScreen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+        image:DecorationImage(
+        image: AssetImage('assets/images/welcome.png'),fit: BoxFit.cover)
+    ),
+    child: Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height*0.6,
+              left: 20,
+              right: 20,
+              ),
+              child: Center(
+                child: Column(
+
+                  children: [
+                    MaterialButton(
+                      height: 80,
+                      minWidth: double.infinity,
+                      color: const Color(0xFF1E232C),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()));
+                      },
+                      child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                      ),
+                    ),
+                    const SizedBox(height: 20,),
+                    MaterialButton(
+                      height: 80,
+                      minWidth:double.infinity,
+                      color: const Color(0xFF1E232C),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterScreen()));
+                      },
+                      child: const Text(
+                          "Register",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                    ),
+
+                  ],
+
+                ),
+              ),
+
+
+            ),
+
+
+          ),
+        ],
+      ),
+
+    ),
+    );
+  }
+}
