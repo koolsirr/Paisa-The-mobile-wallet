9f3dd88deb20bc49d0b10ac94c4dd5a17da0a677
koolsirr <bishalmdr12@gmail.com>
2023-07-27 00:21:13 +0545

    logout

--- lib/Screens/NavBar.dart
+++ lib/Screens/NavBar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:paisa_majorproject/Screens/HomeScreen.dart';
 import 'package:paisa_majorproject/Screens/WelcomeScreen.dart';
 
 class NavBar extends StatefulWidget {
